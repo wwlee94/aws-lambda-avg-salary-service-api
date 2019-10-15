6242c60551a6192db072d506dc4898bc442a1435
wwlee94 <wwlee94@naver.com>
2019-10-15 14:24:01 +0900

    자동 배포 설정1

--- auto_distribute.sh
+++ auto_distribute.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-LAMBDA_FUNC="programmers_lambda_api"
-sudo zip -r programmers_lambda_api.zip ./url/*
-sudo aws lambda update-function-code --function-name $LAMBDA_FUNC --zip-file fileb://programmers_lambda_api.zip
-sudo rm programmers_lambda_api.zip
+LAMBDA_FUNC="aws_lambda_api"
+sudo zip -r aws_lambda_api.zip ./url/*
+sudo aws lambda update-function-code --function-name $LAMBDA_FUNC --zip-file fileb://aws_lambda_api.zip
+sudo rm aws_lambda_api.zip
